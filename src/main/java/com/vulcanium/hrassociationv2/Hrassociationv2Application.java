ca87848214ea54925c235fcb8d43e53e8d93d349
Vulcanium <32834992+Vulcanium@users.noreply.github.com>
2025-09-29 11:43:09 +0200

    Initial commit

---
+++ src/main/java/com/vulcanium/hrassociationv2/Hrassociationv2Application.java
@@ -0,0 +1,13 @@
+package com.vulcanium.hrassociationv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hrassociationv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hrassociationv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vulcanium/hrassociationv2/Hrassociationv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vulcanium.hrassociationv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hrassociationv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
